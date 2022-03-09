605be40b95f28db1618da9caffe40df8e8df73d3
phuel <phuel@uni-bremen.de>
2022-03-09 20:13:27 +0100

    Increased version to 1.2 to match the release number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='tables_extended',
-      version='1.1',
+      version='1.2',
       description='Markdown extension to create tables (with or without header, colspan and rowspan).',
       url='https://github.com/phuel/tables_extended',
       license='BSD License',
